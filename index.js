5be0bacafa610c273a11a1613800aefc3529f204
Chris Riley <chris.riley@cprime.com>
2019-07-23 07:56:12 -0400

    Course retrieval along with basic filtering.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error ('Could not connevt to MongoDB....', err))
+  .catch(err => console.error ('Could not connect to MongoDB....', err))
 
 const courseSchema = new mongoose.Schema({
     name: String,
@@ -18,7 +18,7 @@ async function createCourse(){
     const course = new Course({
         name: 'NodeJS Course',
         author: 'Mosh',
-        tags: ['node','backend'],
+        tags: ['angular','frontend'],
         isPublished: true
     });
 
@@ -26,4 +26,14 @@ async function createCourse(){
     console.log(result);
 }
 
-createCourse();
+async function getCourses(){
+    const courses = await Course
+        .find({author: 'Mosh', isPublished: true})
+        .limit(10)
+        .sort({name: 1})
+        .select({name: 1, tags: 1});
+    console.log(courses);
+}
+
+//createCourse();
+getCourses();
